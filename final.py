0e9e282cc688caefada62336817401023e6a4261
Vincent180877 <vincent180877@gmail.com>
2024-12-20 20:25:14 +0300

    Add files via upload

--- final.py
+++ final.py
@@ -1,12 +1,14 @@
-username=input("Имя пользователя ")
-title=input("Заголовок заметки ")
-title_1=input("Подзаголовок 1 ")
-title_2=input("Подзаголовок 2 ")
-title_3=input("Подзаголовок 3 ")
-title_=[title_1,title_2,title_3]
-content=input("Описание заметки ")
-status=input("Статус заметки ")
-created_date=input("Дата создания заметки:день-месяц-год ")
-issue_date=input("Дата истечения заметки:день-месяц-год ")
-note={"Имя пользователя ":username,"Заголовок заметки ":title,"Описание заметки ":content,"Статус заметки ":status,"Дата создания заметки ":created_date[:5],"Дата истечения заметки ":issue_date[:5],"Подзаголовки ":[title_1,title_2,title_3]}
+username = input("Имя пользователя ")
+title = input("Заголовок заметки ")
+title_1 = input("Подзаголовок 1 ")
+title_2 = input("Подзаголовок 2 ")
+title_3 = input("Подзаголовок 3 ")
+title_ = [title_1, title_2, title_3]
+content = input("Описание заметки ")
+status = input("Статус заметки ")
+created_date = input("Дата создания заметки:день-месяц-год ")
+issue_date = input("Дата истечения заметки:день-месяц-год ")
+note = {"Имя пользователя ": username, "Заголовок заметки ": title, "Описание заметки ": content,
+        "Статус заметки ": status, "Дата создания заметки ": created_date[:5],
+        "Дата истечения заметки ": issue_date[:5], "Подзаголовки ": [title_1, title_2, title_3]}
 print(note)
